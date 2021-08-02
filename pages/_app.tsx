f20e728ffbcdbf2c513995347486b847e02f1690
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-08-02 18:13:07 +0500

    create pages: Portfolios + Categories

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'styles/index.scss'
 import type { AppProps } from 'next/app'
+// import { withLayout } from 'components/Layout'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     // ...
     return <Component {...pageProps} />
 }
 export default MyApp
+// export default withLayout<AppProps>(MyApp)
---
+++ pages/portfolios/edit.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { withLayout } from 'components/Layout'
+
+const Edit = () => {
+    return <div>Edit</div>
+}
+
+export default withLayout(Edit)
